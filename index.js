089230e3b8fb21d799e10e9a5443f22ea04139c1
hrhabib1 <sayefhabib123@gmail.com>
2024-05-05 01:09:35 +0600

    booking added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,7 +29,7 @@ async function run() {
     await client.connect();
 
 const rentServices = client.db('rentHouse').collection('houses');
-
+const bookingCollection = client.db('rentHouse').collection('bookings');
 const buySells = client.db('rentHouse').collection('oldGoods');
 
 const createAdvertisings = client.db('rentHouse').collection('advertising');
@@ -58,6 +58,24 @@ app.get('/buySell', async(req, res) =>{
   const result = await cursor.toArray();
   res.send(result);
 })
+// read renthouse
+app.get('/rentHouses/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const options = {
+      projection: {title: 1, image: 1,  location: 1, price: 1, gender: 1},
+  };
+
+  const result = await rentServices.findOne(query, options);
+  res.send(result);
+})
+// booking
+app.post('/bookings', async(req, res) =>{
+  const addBooking = req.body;
+  console.log(addBooking);
+  const result = await bookingCollection.insertOne(addBooking);
+  res.send(result);
+})
 
 // advertising
 app.post('/createAdvertisings', async(req, res) =>{
@@ -69,6 +87,16 @@ app.post('/createAdvertisings', async(req, res) =>{
 app.get('/createAdvertisings', async(req, res) =>{
   const cursor = createAdvertisings.find();
   const result = await cursor.toArray();
+  res.send(result);
+})
+app.get('/createAdvertisings/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+  const options = {
+      projection: {title: 1, image: 1,  location: 1, price: 1, gender: 1},
+  };
+
+  const result = await createAdvertisings.findOne(query, options);
   res.send(result);
 })
 
